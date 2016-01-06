2fa3a83a7338dada832c46dc4709a5df00f4a69f
IrinaShkviro <IrinaShkviro13@gmail.com>
2016-01-06 08:11:25 +0400

    Add break line trajectory for moving platforms

--- Assets/Script/MovePlatform.cs
+++ Assets/Script/MovePlatform.cs
@@ -1,35 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MovePlatform : MonoBehaviour 
 {
-	public bool direction = true;
-	public float start;
-	public float end;
-	public float speed;
+	public List<Vector3> points = new List<Vector3>();
+	public float speed = 0.05f;
+
 	private new Transform transform;
+	private Vector3 nextPoint;
+	private int currentSector;
+
+	public enum MoveType
+	{
+		Once,
+		Cycle,
+		Partol
+	}
+	public MoveType type;
+
+
+
+	private bool isActive;
+	public bool IsActive
+	{
+		get { return isActive; }
+		set { isActive = value; }
+	}
+
+	public Vector3 GetNextPoint()
+	{
+		float dist = 0;
+		Vector3 newPoint = Vector3.zero; 
+		if (currentSector < points.Count) {
+			dist = Vector3.Distance (nextPoint, points[currentSector]);
+			newPoint = 
+				nextPoint + (points [currentSector] - points [currentSector - 1]).normalized * speed;
+		}
+		if (Vector3.Distance (newPoint, nextPoint) >= dist) {
+			currentSector ++;
+			if (currentSector >= points.Count)
+			{
+				switch(type)
+				{
+				case MoveType.Cycle:
+					currentSector = 1;
+					newPoint = points[0];
+					break;
+				case MoveType.Once:
+					isActive = false;
+					break;
+				case MoveType.Partol:
+					currentSector = 1;
+					points.Reverse();
+					break;
+				}
+			}
+			else
+			{
+				newPoint = nextPoint + 
+					(points [currentSector] - points [currentSector - 1]).normalized * speed;
+			}
+		}
+		nextPoint = newPoint;
+		return nextPoint;
+	}
 
 	void Start()
 	{
 		transform = GetComponent<Transform>();
+		nextPoint = points [0];
+		transform.position = points [0];
+		currentSector = 1;
+		isActive = true;
 	}
 	void Update () 
 	{
-		if (direction)
-		{
-			if (transform.position.x >= end && speed > 0 || transform.position.x <= start && speed < 0)
-			{
-				speed = -speed;
-			}
-			transform.Translate(new Vector3(Time.deltaTime * speed, 0f, 0f));
-		}
-		else
-		{
-			if (transform.position.y >= end && speed > 0 || transform.position.y <= start && speed < 0)
-			{
-				speed = -speed;
-			}
-			transform.Translate(new Vector3(0f, Time.deltaTime * speed, 0f));
+		if (isActive) {
+			transform.position = GetNextPoint ();
 		}
 	}
 }
